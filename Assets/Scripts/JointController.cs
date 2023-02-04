2559071533b5cfe9717496467703ccc569de3da9
Ryan Schaefer <RyanLSchaefer@gmail.com>
2023-02-04 01:14:49 -0600

    JOINTS MATH WORKS

--- Assets/Scripts/JointController.cs
+++ Assets/Scripts/JointController.cs
@@ -1,20 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JointController : MonoBehaviour
 {
-    [SerializeField] private Vector3 v_startTargetRot;
+    private Transform t_this;
+    private Quaternion q_startTargetRot;
+    [SerializeField] private Transform t_targetBone;
 
-    // Start is called before the first frame update
+    private ConfigurableJoint cj_joint;
+    [SerializeField] private bool b_inverted = false;
+
+    private void Awake()
+    {
+        t_this = transform;
+        q_startTargetRot = t_this.localRotation;
+    }
+
     void Start()
     {
         
+        cj_joint = GetComponent<ConfigurableJoint>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!cj_joint) return;
+        if (!t_targetBone) return;
+
+        //t_this.localPosition = t_targetBone.localPosition;
+        //cj_joint.targetRotation = Quaternion.Euler(t_targetBone.localEulerAngles);
+
+        // if (b_inverted)
+        // {
+        //     cj_joint.targetRotation = Quaternion.Inverse(t_targetBone.localRotation * q_startTargetRot);
+        // }
+        // else
+        // {
+        //     cj_joint.targetRotation = t_targetBone.localRotation * q_startTargetRot;
+        // }
+
+        cj_joint.targetRotation = Quaternion.Inverse(t_targetBone.localRotation) * q_startTargetRot;
     }
 }
